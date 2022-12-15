1b99dafd85f6448780ea40c250e117b4dc573d3c
collins1968 <collinsmwendwa1968@gmail.com>
2022-12-15 14:28:31 -0800

    more functions

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -3,42 +3,42 @@
 /**
  * print_triangle - prints a triangle to the screen
  * @size: The size of the triangle
+ *
  * Description: prints a triangle by repeatedly printing a '#'
  * character to the screen many times as dictated by the value
- * of the "size" argument
+ * of the "size" argument.
  * Return: Void
  */
-
 void print_triangle(int size)
 {
-	if (size <= 0)
-	{
-		_putchar('\n');
-	}
-	else
-	{
-		int i;
+  if (size <= 0)
+    {
+      _putchar('\n');
+    }
+  else
+    {
+      int i;
 
-		i = 1;
-		while (i <= size)
-		{
-			int spaces;
-			int hash;
+      i = 1;
+      while (i <= size)
+	{
+	  int spaces;
+	  int hash;
 
-			spaces = sizes - i;
-			hash = i;
-			while (spaces > 0)
-			{
-				_putchar(' ');
-				spaces--;
-			}
-			while (hash > 0)
-			{
-				_putchar('#');
-				hash--;
-			}
-			_putchar('\n');
-			i++;
-		}
+	  spaces = size - i;
+	  hash = i;
+	  while (spaces > 0)
+	    {
+	      _putchar(' ');
+	      spaces--;
+	    }
+	  while (hash > 0)
+	    {
+	      _putchar('#');
+	      hash--;
+	    }
+	  _putchar('\n');
+	  i++;
 	}
+    }
 }
