9e1f2bafe7796aeec3aa85e8ca35ba0db648d6ac
collins1968 <collinsmwendwa1968@gmail.com>
2022-12-15 05:48:50 -0800

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
-#include <main.h>
+#include "main.h"
+
+/**
+ *
+
+ * prints out_putchar followed by a newline.
+
+ *
+
+ * Return: Always 0 (Success)
+
+ */
 
 int main(void)
 {
-	printf("_putchar\n");
+	char ch[8] = "_putchar";
+	int m;
+
+	for (m = 0; m < 8; m++)
+		_putchar(ch[m]);
+
+	_putchar('\n');
+
 	return (0);
 }
--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,4 +1,5 @@
-#include "holberton.h"
+#include <stdio.h>
+#include "main.h"
 
 /**
  * * print_alphabet - Print the alphabet in lowercase.
@@ -11,7 +12,7 @@ void print_alphabet(void)
 	int i;
 	for (i = 97; i < 123; i++)
 	{
-	_putchar(i)
+	_putchar(i);
 	}		
 	_putchar('\n');
 }	
