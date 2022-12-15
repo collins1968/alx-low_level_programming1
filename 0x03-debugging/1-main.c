8f092ba9c3dbaadadebf22c8ca4839d22f1bbd02
collins1968 <collinsmwendwa1968@gmail.com>
2022-12-15 13:19:36 -0800

    more debugging

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - causes an infinite loop
+ * Return: Always 0 (Successful)
+ */
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}
